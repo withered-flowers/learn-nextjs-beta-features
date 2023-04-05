19215ee443811bc7fd573b06ef69f0216c2b23c3
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-04-05 16:56:37 +0700

    feat: initial commit

--- src/app/(form)/formulir/page.tsx
+++ src/app/(form)/formulir/page.tsx
@@ -1,7 +1,15 @@
+import { NextResponse } from "next/server";
+
+const postData = () => {
+  return {};
+};
+
 const FormulirPage = () => {
   return (
     <main className="p-4">
       <h1>Halaman Formulir</h1>
+
+      <form></form>
     </main>
   );
 };
